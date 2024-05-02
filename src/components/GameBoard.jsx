f0a4deb45e2dc06645c9b3e4e3b35bbfd851fd97
Aditya Nur Huda <adityanh1604@gmail.com>
2024-05-02 09:49:02 +0700

    init commit

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function symbolHandler(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const newGameBoard = [...prevGameBoard];
+      newGameBoard[rowIndex][colIndex] =
+        newGameBoard[rowIndex][colIndex] === null ? "X" : "O";
+      return newGameBoard;
+    });
+  }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((symbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => symbolHandler(rowIndex, colIndex)}>
+                  {symbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
---
+++ src/components/Player.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setisEditing] = useState(false);
+
+  function editHandler() {
+    setisEditing((editing) => !editing);
+  }
+
+  function changeHandler(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {isEditing ? (
+          <input type="text" value={playerName} onChange={changeHandler} />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={() => editHandler()}>
+        {isEditing ? "Save" : "Edit"}
+      </button>
+    </li>
+  );
+}
