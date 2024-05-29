e555ca21098cd94679a4985ab92a81be5a05c0c5
Aditya Nur Huda <adityanh1604@gmail.com>
2024-05-29 11:48:31 +0700

    Added log components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
+import Log from "./components/Log";
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
@@ -50,6 +51,7 @@ function App() {
           gameTurns={gameTurns}
         />
       </div>
+      <Log gameTurns={gameTurns} />
     </main>
   );
 }
--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,3 +1,12 @@
-export default function Log() {
-  return <ol></ol>;
+export default function Log({ gameTurns }) {
+  return (
+    <ol id="log">
+      {gameTurns.map((log, index) => (
+        <li
+          key={`${log.square.row}${log.square.col}`}
+          className={index === 0 ? "highlighted" : "undefined"}
+        >{`Player ${log.playerSymbol} placed in coordinate (${log.square.row}, ${log.square.col})`}</li>
+      ))}
+    </ol>
+  );
 }
